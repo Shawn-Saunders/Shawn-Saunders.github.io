77866f36e757c2494f0927b268b6a7b343cda519
Shawn-Saunders <shawn.saunders2@dcmail.ca>
2026-07-17 18:06:01 -0400

    MDN addition
    provided tutorial files from
    the MDN web docs pages added
    
    CSS and JS linked to HTML head tag

--- lab4/part2/scripts/script.js
+++ lab4/part2/scripts/script.js
@@ -4,3 +4,8 @@
     File: script.js
     Javascript for the Interactive gallery of images 
 */
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
