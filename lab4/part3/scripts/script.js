78a48daa08b81468093f8f46f667cad0dd405b90
Shawn-Saunders <shawn.saunders2@dcmail.ca>
2026-07-20 16:10:07 -0400

    Lab 4 part 3 - Final
    Collision detection added and loop updated to call the collision detection each iteration
    
    Final Update, ready for submission

--- lab4/part3/scripts/script.js
+++ lab4/part3/scripts/script.js
@@ -69,6 +69,22 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+
+  // add ball collision
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
 
 // Animating the ball
@@ -100,6 +116,7 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
